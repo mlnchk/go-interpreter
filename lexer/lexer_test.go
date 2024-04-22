3109e2c38db9d046ddf223e46589586d9494e600
mlnchk <danil.mlnchk@gmail.com>
2024-04-22 18:25:38 +0400

    added eq and not eq operators

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -21,11 +21,10 @@ if (5 < 10) {
 	return true;
 } else {
 	return false;
-}`
-	/*
-	   10 == 10;
-	   10 != 9;
-	*/
+}
+
+10 == 10;
+10 != 9;`
 
 	tests := []struct {
 		expectedType    token.TokenType
@@ -96,15 +95,15 @@ if (5 < 10) {
 		{token.FALSE, "false"},
 		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
-		// {token.INT, "10"},
-		// {token.EQ, "=="},
-		// {token.INT, "10"},
-		// {token.SEMICOLON, ";"},
-		// {token.INT, "10"},
-		// {token.NOT_EQ, "!="},
-		// {token.INT, "9"},
-		// {token.SEMICOLON, ";"},
-		// {token.EOF, ""},
+		{token.INT, "10"},
+		{token.EQ, "=="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "10"},
+		{token.NOT_EQ, "!="},
+		{token.INT, "9"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
 	}
 
 	l := New(input)
--- token/token.go
+++ token/token.go
@@ -25,6 +25,8 @@ const (
 	SLASH    = "/"
 	LT       = "<"
 	GT       = ">"
+	EQ       = "=="
+	NOT_EQ   = "!="
 
 	// Delimiters
 	COMMA     = ","
