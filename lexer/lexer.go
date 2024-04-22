40b99b0ab76e20440088edee84c696b358d8bc7c
mlnchk <danil.mlnchk@gmail.com>
2024-04-22 17:50:22 +0400

    skip whitespaces

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -38,6 +38,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -73,6 +75,12 @@ func (l *Lexer) NextToken() token.Token {
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
 
 func (l *Lexer) readIdentifier() string {
