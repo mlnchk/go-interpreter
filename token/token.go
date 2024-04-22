954dc8443ca5c36bd764b0ae18ee56c757213917
mlnchk <danil.mlnchk@gmail.com>
2024-04-22 18:07:03 +0400

    added more one-char tokens

--- token/token.go
+++ token/token.go
@@ -17,8 +17,14 @@ const (
 	INT   = "INT"   // 1343456
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 
 	// Delimiters
 	COMMA     = ","
